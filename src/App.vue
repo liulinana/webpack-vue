d24e0c651b39e859b31c6d75a814a9f70d977ed0
13614477385@163.com <liulina@liulinadeMacBook-Pro.local>
2019-01-29 14:24:05 +0800

    添加axios异步请求

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,82 @@
 <template>
-    <div id="app" :title="message" class="dd" v-if="seen">{{msg}}
-        <router-view></router-view>
-        <router-link to="/table/1">表格</router-link>
-        <router-link to="/form">表单</router-link>
-        <ol>
-            <li v-for="todo in todos">{{todo.text}}</li>
-        </ol>
-        <p>{{aa}}</p>
-        <input v-model="aa">
-        <Button type="primary" @click="reverseMessage">逆转消息</Button>
+    <div class="layout">
+        <Row type="flex">
+            <Col span="5" class="layout-menu-left">
+                <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']" @on-select="openView">
+                    <div class="layout-logo-left"></div>
+                    <Submenu name="1">
+                        <template slot="title">
+                            <Icon type="ios-navigate"></Icon>
+                            导航一
+                        </template>
+                        <MenuItem name="form">form</MenuItem>
+                        <MenuItem name="table">table</MenuItem>
+                        <MenuItem name="1-3">选项 3</MenuItem>
+                    </Submenu>
+                    <Submenu name="2">
+                        <template slot="title">
+                            <Icon type="ios-keypad"></Icon>
+                            导航二
+                        </template>
+                        <MenuItem name="2-1">选项 1</MenuItem>
+                        <MenuItem name="2-2">选项 2</MenuItem>
+                    </Submenu>
+                    <Submenu name="3">
+                        <template slot="title">
+                            <Icon type="ios-analytics"></Icon>
+                            导航三
+                        </template>
+                        <MenuItem name="3-1">选项 1</MenuItem>
+                        <MenuItem name="3-2">选项 2</MenuItem>
+                    </Submenu>
+                </Menu>
+            </Col>
+            <Col span="19">
+                <div class="layout-header"></div>
+                <div class="layout-breadcrumb">
+                    <Breadcrumb>
+                        <Breadcrumb :style="{margin: '24px 0'}">
+                            <!--<BreadcrumbItem v-for="item in breadcrumbItem" >{{item}}</BreadcrumbItem>-->
+                            <BreadcrumbItem>首页</BreadcrumbItem>
+                        </Breadcrumb>
+                    </Breadcrumb>
+                </div>
+                <div class="layout-content">
+                    <div class="layout-content-main">
+                        <router-view></router-view>
+                    </div>
+                </div>
+                <div class="layout-copy">
+                    2011-2016 &copy; TalkingData
+                </div>
+            </Col>
+        </Row>
     </div>
 </template>
-<style>
-    .dd{}
-</style>
 <script>
-    export default{
-        name: "App",
-        data(){
+    export default {
+        data () {
             return{
-                msg: 'fff!',
-                message:"sss",
-                seen:true,
-                todos: [
-                    { text: '学习 JavaScript' },
-                    { text: '学习 Vue' },
-                    { text: '整个牛项目' }
-                ],
-                aa:"Hello Vue.js!",
+                openItem:[],
+                open:[],
             }
         },
+        computed: {
+            breadcrumbItem(){
+                return this.open.concat(this.openItem)
+            }
+
+        },
         methods: {
-            reverseMessage: function () {
-                this.todos.push({ text: '新项目' });
-                this.aa = this.aa.split('').reverse().join('')
+            openView(e){
+                this.$router.push({
+                    name:e
+                })
+
             }
         }
-
     }
 </script>
+<style lang="scss">
+    @import "./style/style";
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 import router from './router';
 import App from "./App";
+import api from './axios/api.js'
+Vue.prototype.$api = api;
 
 Vue.use(iView);
 let app = new Vue({
